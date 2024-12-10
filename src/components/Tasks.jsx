9b3fbdedc8d1d53b882fb5ff9dc81c571d52bfca
Marcio Costa <marcio-costa38@hotmail.com>
2024-12-10 17:21:45 -0300

    criando pagina de visualizacao de detalhes

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,21 +1,36 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(tasks) {
+    const query = new URLSearchParams();
+    query.set("title", tasks.title);
+    query.set("description", tasks.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((tasks) => (
         <li key={tasks.id} className="flex gap-2">
           <button
-            onClick={() => onTaskClick(tasks.id)}
+            onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
               tasks.isCompleted && `line-through`
             }`}
           >
             {tasks.title}
           </button>
-          <button className="bg-slate-400 text-white p-2 rounded-md">
+
+          <button
+            onClick={() => onSeeDetailsClick(tasks)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
             <ChevronRightIcon />
           </button>
+
           <button
             onClick={() => onDeleteTaskClick(tasks.id)}
             className="bg-slate-400 text-white p-2 rounded-md"
