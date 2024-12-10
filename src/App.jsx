d5d7e34c4f49a093db46f96b24bdedd26259f47e
Marcio Costa <marcio-costa38@hotmail.com>
2024-12-10 00:37:01 -0300

    criando a funcao de adicioar e deletar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -35,15 +36,30 @@ function App() {
     });
     setTasks(newTasks)
   }
+  
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((tasks) => tasks.id !== taskId)
+    setTasks(newTasks)
+  }
+
+  function onAddTaskSubmit(title, description){
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    }
+    setTasks([...tasks, newTask])
+  }
 
   return (
     <div className="h-screen w-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
   );
