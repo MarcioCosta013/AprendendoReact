48eb37afe4c135052bbcc65943792860587094ff
Marcio Costa <marcio-costa38@hotmail.com>
2024-12-16 20:53:19 -0300

    corrigindo bug dos titulos

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import Title from "../components/Title";
 
 function TaskPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
@@ -12,12 +12,13 @@ function TaskPage() {
     <div className="h-screen w-screen bg-slate-500 flex p-6 justify-center">
       <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative mb-6">
-            <button onClick={()=> navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
-                <ChevronLeftIcon />
-            </button>
-          <Title>
-            Detalhes da Tarefas
-          </Title>
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <Title>Detalhes da Tarefas</Title>
         </div>
 
         <div className="bg-slate-200 p-4 rounded-md">
